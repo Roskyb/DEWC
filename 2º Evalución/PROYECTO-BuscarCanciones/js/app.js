7946d388664b905babcc137261a1b1c27e5aa3ec
Jorge Blanco <jorge.blanco.gil.00@gmail.com>
2021-11-26 09:01:46 +0100

    Ejercicio buscar canciones

--- 2º Evalución/PROYECTO-BuscarCanciones/js/app.js
+++ 2º Evalución/PROYECTO-BuscarCanciones/js/app.js
@@ -1,20 +1,62 @@
 let btnEnviar = document.querySelector('.enviar')
 
-btnEnviar.addEventListener('click', (e) => {
-	e.preventDefault()
-	let inputArtista = document.getElementById('artista') 
-	let inputCancion = document.getElementById('cancion')
-
-	if(!inputArtista) return
-	if(!inputCancion) return 
-
-	fetch(`https://api.lyrics.ovh/v1/${inputArtista.value}/${inputCancion.value}`)
+const  buscarCancion = async (inputArtista, inputCancion) => {
+	let loader = parseTextToNode(`<div class="spinner-border text-primary" role="status">
+	<span class="sr-only">Loading...</span></div>`)
+	let container = document.querySelector('.letra-resultado')
+	container.appendChild(loader)
+	fetch(`https://api.lyrics.ovh/v1/${inputArtista}/${inputCancion}`)
 		.then(response => response.json())
 		.then(response => {
-			if(response.error) document.getElementById('resultado').innerHTML = '<p class="text-warning">No se han encontrado resultados</p>'
-			else {
+			if (response.error) {
+				document.getElementById('resultado').innerHTML = `
+				<div class="alert alert-danger" role="alert">
+					No se ha encontrado la cancion que buscabas!
+				</div>`
+				document.getElementById('titulo-cancion').innerHTML = ''
+
+			} else {
 				document.getElementById('resultado').innerHTML = response.lyrics
+				document.getElementById('titulo-cancion').innerHTML = inputCancion
+				loader.remove()
 			}
 		})
+}
+
+const parseTextToNode = (nodeString) => {
+	let doc = new DOMParser().parseFromString(nodeString, 'text/html')
+	return doc.body.firstChild
+}
+const limpiarErrores = () => {
+	document.querySelectorAll('p.text-warning').forEach(e => e.remove())
+}
+
+
+btnEnviar.addEventListener('click', async (e) => {
+	e.preventDefault()
+	let inputArtista = document.getElementById('artista')
+	let inputCancion = document.getElementById('cancion')
+
+	if (!inputArtista.value) {
+		let elementArtista = document.getElementById('artista')
+		let errorMsg = parseTextToNode('<p class="text-warning">Deves introducir un autor</p>', 'text/html')
+		elementArtista.parentElement.appendChild(errorMsg)
+		return
+
+	}
+	if (!inputCancion.value) {
+		let elementArtista = document.getElementById('cancion')
+		let errorMsg = parseTextToNode('<p class="text-warning">Deves introducir una cancion</p>', 'text/html')
+		elementArtista.parentElement.appendChild(errorMsg)
+		return
+	}
+
+	limpiarErrores()
+
+	buscarCancion(inputArtista.value, inputCancion.value)
+
+
+
+
 
 })
